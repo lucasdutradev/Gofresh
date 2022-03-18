05fdc5286fb0df507d925a9f6445dfd6a33a34b1
Carlos Henrique <carloshenriquesantanas19@gmail.com>
2022-03-18 15:37:37 -0300

    feat(js): Header Component Implemented

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import Provider from "./Providers";
-import { DefaultStyles } from "./styles";
+import GlobalStyle from "./styles";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <DefaultStyles />
-    <Provider>
-      <ToastContainer />
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <GlobalStyle />
+        <Provider>
+            <ToastContainer />
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
