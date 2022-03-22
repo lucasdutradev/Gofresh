533c0adce9dda062257d9b3604148ec4721ab08f
Babi <babiferreirab15@gmail.com>
2022-03-22 21:43:34 +0100

    feat(js): resolve conflicts

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -4,19 +4,13 @@ import { useContext } from "react";
 import { CartContext } from "../../Providers/cart";
 import { useHistory } from "react-router-dom/";
 const Cart = ({ data }) => {
-<<<<<<< HEAD
-  // const [cart, setCart] = useState([]);
-  const { cart } = useContext(CartContext);
-  const history = useHistory();
-=======
   const { cart, pushProduct } = useContext(CartContext);
   console.log(cart);
->>>>>>> e863958acbdc41f8a932e97ca393e5acf21d56d5
   return (
     <ContainerCart>
       <div className="containerBody">
         <div className="containerHeader">
-          <button onClick={() => history.push("/menu")}>ir para lista</button>
+          <button>ir para lista</button>
         </div>
         <div className="containerCart">
           <h1>Carrinho</h1>
