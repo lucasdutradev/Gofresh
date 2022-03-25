9b39a8eb642215ccfa30343663220c3724a3174e
LucasDutra <lucasbryan0217@gmail.com>
2022-03-24 22:01:18 -0300

    changes

--- src/Providers/home/index.js
+++ src/Providers/home/index.js
@@ -3,37 +3,37 @@ import { createContext, useState } from "react";
 export const HomeContext = createContext([]);
 
 export const HomeProvider = ({ children }) => {
-    const [count, setCount] = useState(0);
-    const [modal, setModal] = useState(false);
+  const [count, setCount] = useState(0);
+  const [modal, setModal] = useState(false);
 
-    const rating = [
-        { title: "Título 1", description: "Descrição 1" },
-        { title: "Título 2", description: "Descrição 2" },
-        { title: "Título 3", description: "Descrição 3" },
-        { title: "Título 4", description: "Descrição 4" },
-    ];
+  const rating = [
+    { title: "Título 1", description: "Descrição 1" },
+    { title: "Título 2", description: "Descrição 2" },
+    { title: "Título 3", description: "Descrição 3" },
+    { title: "Título 4", description: "Descrição 4" },
+  ];
 
-    const addCount = () => {
-        if (count < 3) {
-            setCount(count + 1);
-        } else {
-            setCount(0);
-        }
-    };
+  const addCount = () => {
+    if (count < 3) {
+      setCount(count + 1);
+    } else {
+      setCount(0);
+    }
+  };
 
-    const subCount = () => {
-        if (count > 0) {
-            setCount(count - 1);
-        } else {
-            setCount(3);
-        }
-    };
+  const subCount = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    } else {
+      setCount(3);
+    }
+  };
 
-    return (
-        <HomeContext.Provider
-            value={{ rating, count, addCount, subCount, modal, setModal }}
-        >
-            {children}
-        </HomeContext.Provider>
-    );
+  return (
+    <HomeContext.Provider
+      value={{ rating, count, addCount, subCount, modal, setModal }}
+    >
+      {children}
+    </HomeContext.Provider>
+  );
 };
